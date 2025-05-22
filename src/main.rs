f38dd5ccf87a202f9c0a6cb1da6618325a1afa73
joseporcar <joseaporcar@gmail.com>
2025-05-22 08:29:10 -0400

    renaming and formatting

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 fn main() {
     let center = Point::center();
-    let n = 100000;
-    let mut c = 0;
-    for _ in 0..n {
+    let total_beans = 100000;
+    let mut within_circle = 0;
+    for _ in 0..total_beans {
         if Point::new().in_circle(&center) {
-            c += 1; 
+            within_circle += 1;
         }
     }
-    println!("total beans = {n}\nWithin Circle = {c}\nArea (radius is 0.5) = {}", c as f64 / n as f64)
+    println!(
+        "total beans = {total_beans}\nWithin Circle = {within_circle}\nArea (radius is 0.5) = {}",
+        within_circle as f64 / total_beans as f64
+    )
 }
-
 
 #[derive(Debug)]
 struct Point(f64, f64);
@@ -25,11 +27,10 @@ impl Point {
     fn distance(&self, other: &Point) -> f64 {
         let Point(x_p1, y_p1) = self;
         let Point(x_p2, y_p2) = other;
-        ((x_p1-x_p2).powi(2) + (y_p1-y_p2).powi(2)).sqrt()
+        ((x_p1 - x_p2).powi(2) + (y_p1 - y_p2).powi(2)).sqrt()
     }
 
     fn in_circle(&self, center: &Point) -> bool {
         self.distance(center) < 0.5
     }
 }
-
